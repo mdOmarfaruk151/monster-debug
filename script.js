d16fcc3348fc13b1b01982976fb9a50eb432c8c5
mdOmarfaruk151 <mdomar28151@gmile.com>
2022-10-13 22:24:26 +0600

    solve mistakes count

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const modalBackground = document.getElementById("modal-background");
 // variables
 let userText = "";
 let errorCount = 0;
-let startTime ;
+let startTime;
 let questionText = "";
 
 // Load and display question
@@ -35,30 +35,41 @@ const typeController = (e) => {
 
   // if it is not a valid character like Control/Alt then skip displaying anything
   if (!validLetters.includes(newLetter)) {
+    
     return;
   }
+  
 
   userText += newLetter;
 
   const newLetterCorrect = validate(newLetter);
 
   if (newLetterCorrect) {
-    display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
+    display.innerHTML += `<span class="green">${
+      newLetter === " " ? "▪" : newLetter
+    }</span>`;
   } else {
-    display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    display.innerHTML += `<span class="red">${
+      newLetter === " " ? "▪" : newLetter
+    }</span>`;
   }
+
 
   // check if given question text is equal to user typed text
   if (questionText === userText) {
     gameOver();
   }
+ 
 };
 
 const validate = (key) => {
   if (key === questionText[userText.length - 1]) {
     return true;
   }
-  return false;
+  // return false;
+  else{
+    errorCount += 1
+  }
 };
 
 // FINISHED TYPING
@@ -89,7 +100,7 @@ const gameOver = () => {
 
   // restart everything
   startTime = null;
-  errorCount = "";
+  errorCount = 0;
   userText = "";
   display.classList.add("inactive");
 };
@@ -118,7 +129,6 @@ const start = () => {
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
-      
     }
     count--;
   }, 1000);
@@ -128,13 +138,14 @@ const start = () => {
 startBtn.addEventListener("click", start);
 
 // If history exists, show it
-displayHistory();
+// displayHistory();
 
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent.toFixed(0) : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${
+    startTime ? timeSpent.toFixed(0) : 0
+  } seconds`;
 }, 1000);
